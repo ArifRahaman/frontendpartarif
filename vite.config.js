e61a1c793bdc57555ef74cd9c1c1c65181059187
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2025-07-05 00:28:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,  // Set the port to 5173
     host: '0.0.0.0',  // Ensure it binds to all network interfaces
-    allowedHosts:"frontendpartarif.onrender.com"
+    allowedHosts:['frontendpartarif.onrender.com']
   },
       // allowedHosts: ['']
 });
